0a3ce516c61784bb6b9bbe9fa9dd01b4d6f5caae
Keren Cerezo <kerencerezo@Kerens-MacBook.local>
2018-10-07 18:24:48 -0400

    changes

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Meat Burger', false);
